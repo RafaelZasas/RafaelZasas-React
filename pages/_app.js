1ae6a10a8203cc6aa658ea5ec1db1a7278374711
Rafael Zasas <zasas.rafi@gmail.com>
2021-08-15 21:40:56 -0700

    Initial Setup + Navbar
    ➕ tailwind CSS ^2.0.2
    Set up basic Nav Bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import {Navbar} from '../components/Navbar'
 import '../styles/globals.css'
+import '../styles/main.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Navbar />
+    <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
