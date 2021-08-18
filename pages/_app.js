236b57a2c20b7f3f25b6da3c661c8423e2a4268a
Rafael Zasas <zasas.rafi@gmail.com>
2021-08-18 02:02:20 -0700

    Add auth context to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,20 @@ import '../styles/globals.css'
 import '../styles/main.css';
 import {Navbar} from '../components/Navbar'
 import Footer from "../components/Footer";
+import {UserContext} from "../lib/context";
 
 function MyApp({Component, pageProps}) {
     return (
-        <div className='flex flex-col min-h-screen'>
-        <Navbar/>
-        <main className='flex-grow' >
-            <Component {...pageProps} />
-        </main>
-        <Footer/>
-        </div>
+        <UserContext.Provider
+            value={{user:{}, username: 'raff'}}>
+            <div className='flex flex-col min-h-screen'>
+                <Navbar/>
+                <main className='flex-grow' >
+                    <Component {...pageProps} />
+                </main>
+                <Footer/>
+            </div>
+        </UserContext.Provider>
     );
 }
 
