599d29af3f8475562b3430850b292179ef46ff4b
Rafael Zasas <zasas.rafi@gmail.com>
2021-08-19 11:58:19 -0700

    Add interface types; rename context.tsx -> .ts

---
+++ lib/types.ts
@@ -0,0 +1,18 @@
+export interface User {
+    // describes the data that a user is expected expected to contain
+    uid: string;
+    bio?: string;
+    email: string;
+    photoURL?: string;
+    profilePhoto?: string;
+    username?: string;
+    permissions: {
+        user: boolean,
+        edit: boolean,
+        admin: boolean
+    };
+    communications?: {
+        email: { comments: boolean, projects: boolean, updates: boolean },
+        push: { comments: boolean, projects: boolean, updates: boolean }
+    }
+}
