8095dd929e460fe63c949f21a895050ab805535f
Rafael Zasas <zasas.rafi@gmail.com>
2021-08-25 21:25:21 -0700

    Add Background gradient to whole app

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({Component, pageProps}) {
     return (
         <UserContext.Provider
             value={userData}>
-            <div className='flex flex-col min-h-screen'>
+            <div className='flex flex-col min-h-screen bg-gradient-to-tl from-blue-200 via-blue-100 via-blue- to-transparent'>
                 <Navbar/>
                 <main className='flex-grow' >
                     <Component {...pageProps} />
