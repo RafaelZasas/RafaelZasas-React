03c57a0a62b8cb6da3e5c4d4c64e9ed26b2c77a3
Rafael <zasas.rafi@gmail.com>
2022-03-07 00:01:34 -0800

    🚚 (Editor): Move initial state to its own file

---
+++ components/textEditor/InitialEditorState.tsx
@@ -0,0 +1,13 @@
+export const initialState = {
+  entityMap: {},
+  blocks: [
+    {
+      text: '',
+      key: 'foo',
+      type: 'unstyled',
+      entityRanges: [],
+      depth: 1,
+      inlineStyleRanges: [],
+    },
+  ],
+};
