8cbbcbad79cd11db6889972e057940785af816da
Rafael <zasas.rafi@gmail.com>
2022-03-13 21:44:34 -0700

    🚸 (Blog Page): Update styling and remove unused imports

--- pages/admin/blog.tsx
+++ pages/admin/blog.tsx
@@ -1,16 +1,12 @@
-import {convertFromRaw, convertToRaw, EditorState} from 'draft-js';
+import {EditorState} from 'draft-js';
 import React from 'react';
-import EditorContent from '../../components/textEditor/EditorContent';
-import {initialState} from '../../components/textEditor/InitialEditorState';
 import TextEditor from '../../components/textEditor/TextEditor';
 
 export default function BlogPage({}) {
-  const [editorState, setEditorState] = React.useState(() =>
-    EditorState.createWithContent(convertFromRaw(initialState))
-  );
+  const [editorState, setEditorState] = React.useState(() => EditorState.createEmpty());
 
   return (
-    <div className="flex min-h-screen flex-col justify-items-center p-4 align-middle">
+    <div className="flex min-h-screen flex-col justify-items-center p-0.5 align-middle md:p-4">
       <h2 className="text-center text-lg font-semibold">Add Blog Entry</h2>
       <div className="flex-row">
         <div className="container mx-auto py-2 sm:px-6 lg:px-8">
