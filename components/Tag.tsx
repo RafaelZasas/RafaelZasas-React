c73b2a09d308558d23058705d1341ec8edd304a0
Rafael <zasas.rafi@gmail.com>
2022-03-17 23:03:26 -0700

    💩 (Tag Component): Shity fix for colors not rendering

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -9,14 +9,17 @@ interface TagProps {
   function?: Function;
 }
 
+function _getBackground(tagColor: string) {
+  const colors = ['bg-blue-500', 'bg-fuchsia-500', 'bg-rose-500', 'bg-yellow-500', 'bg-red-500', 'bg-emerald-500'];
+  const match = colors.filter((item) => item === tagColor);
+  return match.length > 0 ? match[0] : 'bg-sky-500';
+}
+
 export default function Tag(props: TagProps) {
-  // todo: use tag colors from db in way that doesnt break.
+  const classes = ` inline-flex cursor-pointer items-center rounded-full border-2 border-slate-400/40
+        bg-opacity-80 px-2.5 py-0.5 text-sm font-medium text-gray-200 hover:bg-opacity-100 md:mt-2 lg:mt-0`;
   return (
-    <div
-      onClick={() => props.function(props.tag)}
-      className={`inline-flex cursor-pointer items-center rounded-full border-2 border-${props.tag.color}-600/30
-        bg-${props.tag.color}-500/70 px-2.5 py-0.5 text-sm font-medium text-gray-200 hover:bg-${props.tag.color}-500 md:mt-2 lg:mt-0`}
-    >
+    <div onClick={() => props.function(props.tag)} className={`${classes} ${_getBackground(props.tag.color)}`}>
       {props.tag.name}
     </div>
   );
