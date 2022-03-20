bbeb6c1af2c5f330b6a9e3f64b65e2ef29ce18f3
Rafael <zasas.rafi@gmail.com>
2022-03-19 19:14:00 -0700

    👽️ (Firebase Init): Add firebase V9 for some modules

--- lib/context.ts
+++ lib/context.ts
@@ -1,6 +1,6 @@
-import { createContext } from 'react';
-import firebase from "firebase";
+import {createContext} from 'react';
+import firebase from 'firebase/compat';
 import {User} from './types';
 let userData: User = null;
 let user: firebase.User = null;
-export const UserContext = createContext({ user , userData });
+export const UserContext = createContext({user, userData});
