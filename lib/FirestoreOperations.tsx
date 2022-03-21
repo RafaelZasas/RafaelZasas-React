5d6495213ff5224f3554204165fd271d2a2e8f2d
Rafael <zasas.rafi@gmail.com>
2022-03-20 19:33:11 -0700

    ✨ (Firestore Operations): Add function to retrieve all blog posts

--- lib/FirestoreOperations.tsx
+++ lib/FirestoreOperations.tsx
@@ -23,3 +23,19 @@ export const PostBlog = (data: BlogPost) => {
   const blogRef = firestore.collection('blogs');
   blogRef.add(data);
 };
+
+export const GetBlogPosts = async () => {
+  const postsRef = firestore.collection('blogs').limit(20).orderBy('createdAt', 'desc');
+  const posts = (await postsRef.get()).docs.map((doc) => {
+    const data: BlogPost = doc.data() as BlogPost;
+    if ('seconds' in data.createdAt) {
+      return {
+        ...data,
+        id: doc.id,
+        createdAt: data?.createdAt?.seconds || 0,
+        updatedAt: data?.updatedAt?.seconds || 0,
+      };
+    }
+  });
+  return posts;
+};
