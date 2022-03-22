251d8c7fe37669674cd3408a7f8c02a3ec61eebb
Rafael <zasas.rafi@gmail.com>
2022-03-22 12:29:10 -0700

    💄 (Tailwind Config): Add google roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,26 @@ module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+    extend: {
+      fontFamily: {
+        roboto: [
+          'Roboto',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji',
+        ],
+      },
+    },
     listStyleType: {
       roman: 'upper-roman',
       disc: 'disc',
