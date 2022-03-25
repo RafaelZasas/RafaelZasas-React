0667eae233a9557f558993763a36d37b8ca8ae3e
Rafael <zasas.rafi@gmail.com>
2022-03-25 00:44:10 -0700

    ✨ (Blog Comments): Use database comments for comment section and imrove ux for adding comments

--- components/BlogCommentItem.tsx
+++ components/BlogCommentItem.tsx
@@ -1,22 +1,26 @@
-import {UserData} from '../lib/types';
+import {BlogComment, UserData} from '../lib/types';
 import Link from 'next/link';
 import CustomImage from './Image';
+import EditorContent from './textEditor/EditorContent';
+import {convertFromRaw, EditorState} from 'draft-js';
 
 const UpVoteIcon = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="24" height="24" fill="currentColor">
-      <path
-        d="M9.39 265.4l127.1-128C143.6 131.1 151.8 128 160 128s16.38 3.125 22.63 9.375l127.1 128c9.156
+    <div className="mx-auto flex cursor-pointer items-center justify-center text-center">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="24" height="24" fill="currentColor">
+        <path
+          d="M9.39 265.4l127.1-128C143.6 131.1 151.8 128 160 128s16.38 3.125 22.63 9.375l127.1 128c9.156
             9.156 11.9 22.91 6.943 34.88S300.9 320 287.1 320H32.01c-12.94 0-24.62-7.781-29.58-19.75S.2333
             274.5 9.39 265.4z"
-      />
-    </svg>
+        />
+      </svg>
+    </div>
   );
 };
 
 const DownVoteIcon = () => {
   return (
-    <div className="mx-auto flex items-center justify-center text-center text-blue-500">
+    <div className="mx-auto flex cursor-pointer items-center justify-center text-center text-blue-500">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="24" height="24" fill="currentColor">
         <path
           d="M310.6 246.6l-127.1 128C176.4 380.9 168.2 384 160 384s-16.38-3.125-22.63-9.375l-127.1-128C.2244
@@ -41,42 +45,37 @@ const VoteSection = (props: VoteSectionProps) => {
   );
 };
 
-const ReputationSection = (props: {user: UserData}) => {
+const ReputationSection = (props: {user: Partial<UserData>}) => {
   let reputation = '';
   if (props.user?.permissions?.admin) {
     reputation = 'admin';
   } else {
-    reputation = `${props.user?.permissions?.level}` ?? '0';
+    reputation = `${props.user?.permissions?.level || 0}`;
   }
 
   return <p className="my-0 px-1 text-xs text-slate-500">Reputation: {reputation}</p>;
 };
 
-const UserSection = (props: {user: UserData}) => {
+const UserSection = (props: {user: Partial<UserData>}) => {
+  const defaultAvatar = `https://firebasestorage.googleapis.com/v0/b/rafael-zasas.appspot.com/o/default-avatar.jpg?alt=media&token=da5befb3-193c-4a14-a17b-f036828dbf5b`;
   return (
     <div className="grid grid-cols-1">
       <p className="text-xs text-slate-500">Replied Mar 22 2021 at 14:23</p>
       <div className="flex flex-row">
         <div className="justify-right">
-          {props.user?.profilePhoto || props.user?.photoURL ? (
-            <CustomImage
-              src={props.user?.profilePhoto ?? props.user?.photoURL}
-              alt={props.user?.username}
-              width={40}
-              height={40}
-              className="pointer-events-none object-cover"
-            />
-          ) : (
-            <svg className="h-20 w-20 text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-            </svg>
-          )}
+          <CustomImage
+            src={props.user?.profilePhoto || props.user?.photoURL || defaultAvatar}
+            alt={props.user?.username ?? 'profile photo'}
+            width={40}
+            height={40}
+            className="pointer-events-none object-cover"
+          />
         </div>
         <div className="flex flex-col">
           {/* Username Section */}
           <Link passHref href={`/users/${props.user?.uid}`}>
             <a className="my-0 cursor-pointer self-start px-1 pb-0 pt-0.5 text-sm text-blue-500 visited:text-purple-500 hover:text-blue-400">
-              {props.user?.username ?? 'Username'}
+              {props.user?.username ?? props.user?.email.split('@')[0]}
             </a>
           </Link>
 
@@ -89,27 +88,29 @@ const UserSection = (props: {user: UserData}) => {
 };
 
 interface BlogCommentProps {
-  upVotes: number;
-  downVotes: number;
-  content: string;
+  comment: BlogComment;
   user: UserData;
 }
 
-export default function BlogComment(props: BlogCommentProps) {
+export default function BlogCommentItem(props: BlogCommentProps) {
+  const contentState = convertFromRaw(JSON.parse(props.comment.body));
+  const editorState = EditorState.createWithContent(contentState);
   return (
     <div className="flex flex-col space-y-3 py-2 md:space-y-4">
       <div className="flex flex-row space-x-2 ">
-        <VoteSection numVotes={props.upVotes - props.downVotes} />
-        <p className="text-left">{props.content}</p>
+        <VoteSection numVotes={props.comment?.upVotes.length - props.comment?.downVotes.length} />
+        <EditorContent editorState={editorState} />
       </div>
       <div className="grid max-h-16 grid-cols-2">
         <div className="mx-2 flex flex-row items-start justify-start space-x-2 text-base text-slate-500">
-          <p className="cursor-pointer">reply</p>
-          <p className="cursor-pointer">edit</p>
-          <p className="cursor-pointer">flag</p>
+          {props.user && <p className="cursor-pointer">reply</p>}
+
+          {(props.user?.permissions?.admin || props.user?.permissions?.level > 5) && (
+            <p className="cursor-pointer">flag</p>
+          )}
         </div>
         <div className="mx-2 justify-self-end">
-          <UserSection user={props.user} />
+          <UserSection user={props.comment.author} />
         </div>
       </div>
     </div>
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,10 +1,17 @@
-import {convertFromRaw, EditorState} from 'draft-js';
+import {ContentState, convertFromRaw, convertToRaw, EditorState} from 'draft-js';
 import Metatags from '../../components/Metatags';
 import EditorContent from '../../components/textEditor/EditorContent';
-import {GetBlogPost, GetBlogPosts} from '../../lib/FirestoreOperations';
-import {BlogPost, UserData} from '../../lib/types';
+import {addBlogComment, getBlogComments, GetBlogPost, GetBlogPosts} from '../../lib/FirestoreOperations';
+import {BlogComment, BlogPost, UserData} from '../../lib/types';
 import {UserInfo} from '@firebase/auth-types';
-import BlogComment from '../../components/BlogComment';
+import TextEditor from '../../components/textEditor/TextEditor';
+import {Dispatch, SetStateAction, useState} from 'react';
+import Button from '../../components/Button';
+import {serverTimestamp} from 'firebase/firestore';
+import {faFeather, faReply} from '@fortawesome/free-solid-svg-icons';
+import {Toast, ToastData} from '../../components/toast';
+import Link from 'next/link';
+import BlogCommentItem from '../../components/BlogCommentItem';
 
 export async function getStaticProps({params}) {
   console.log(params);
@@ -12,11 +19,12 @@ export async function getStaticProps({params}) {
   const {slug} = params;
 
   const post = await GetBlogPost(slug);
+  const comments = await getBlogComments(slug);
 
   const path = post?.id ?? 'not-found';
 
   return {
-    props: {post, path},
+    props: {post, path, comments},
     revalidate: 5000,
   };
 }
@@ -44,77 +52,19 @@ export async function getStaticPaths() {
 interface BlogPostProps {
   path: string;
   post: BlogPost;
+  comments: BlogComment[];
   userProps: {user: UserInfo; userData: UserData};
 }
 
 export default function BlogPostPage(props: BlogPostProps) {
   const contentState = convertFromRaw(JSON.parse(props.post.body));
   const editorState = EditorState.createWithContent(contentState);
+  const [showCommentEditor, setShowCommentEditor] = useState<boolean>(false);
+  const [showToast, setShowToast] = useState<boolean>(false);
+  const [toastData, setToastData] = useState<ToastData>();
 
-  const comments = [
-    {
-      upVotes: 6,
-      downVotes: 2,
-      content: `This is a fantastic comment. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-         Maecenas lectus nulla, tempor vel facilisis eu, facilisis sit amet enim.
-          Nullam enim dui, dictum non laoreet at, cursus id diam.`,
-    },
-    {
-      upVotes: 20,
-      downVotes: 2,
-      content: `This is another fantastic comment. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-         Maecenas lectus nulla, tempor vel facilisis eu, facilisis sit amet enim.
-          Nullam enim dui, dictum non laoreet at, cursus id diam.`,
-    },
-    {
-      upVotes: 16,
-      downVotes: 7,
-      content: `This is not a very good comment. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-         Maecenas lectus nulla, tempor vel facilisis eu, facilisis sit amet enim.
-          Nullam enim dui, dictum non laoreet at, cursus id diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-         Maecenas lectus nulla, tempor vel facilisis eu, facilisis sit amet enim.
-          Nullam enim dui, dictum non laoreet at, cursus id diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-         Maecenas lectus nulla, tempor vel facilisis eu, facilisis sit amet enim.
-          Nullam enim dui, dictum non laoreet at, cursus id diam.`,
-    },
-    {
-      upVotes: 7,
-      downVotes: 3,
-      content: `This is another not a very good comment. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-         Maecenas lectus nulla, tempor vel facilisis eu, facilisis sit amet enim.
-          Nullam enim dui, dictum non laoreet at, cursus id diam.`,
-    },
-    {
-      upVotes: 26,
-      downVotes: 7,
-      content: `I believe it should be like x y z. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-         Maecenas lectus nulla, tempor vel facilisis eu, facilisis sit amet enim.
-          Nullam enim dui, dictum non laoreet at, cursus id diam.`,
-    },
-    {
-      upVotes: 16,
-      downVotes: 7,
-      content: `I believe it should instead be a b c. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-         Maecenas lectus nulla, tempor vel facilisis eu, facilisis sit amet enim.
-          Nullam enim dui, dictum non laoreet at, cursus id diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-         Maecenas lectus nulla, tempor vel facilisis eu, facilisis sit amet enim.
-          Nullam enim dui, dictum non laoreet at, cursus id diam.`,
-    },
-    {
-      upVotes: 7,
-      downVotes: 3,
-      content: `My opinion is such and such. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-         Maecenas lectus nulla, tempor vel facilisis eu, facilisis sit amet enim.
-          Nullam enim dui, dictum non laoreet at, cursus id diam.`,
-    },
-    {
-      upVotes: 26,
-      downVotes: 7,
-      content: `I dont really have an opinion on this. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-         Maecenas lectus nulla, tempor vel facilisis eu, facilisis sit amet enim.
-          Nullam enim dui, dictum non laoreet at, cursus id diam.`,
-    },
-  ];
+  const usersComment = props.comments.filter((comment) => comment.id === props.userProps?.user?.uid);
+
   return (
     <main>
       <Metatags
@@ -126,34 +76,173 @@ export default function BlogPostPage(props: BlogPostProps) {
         }
         currentURL={`rafaelzasas.com/blog/${props.path}`}
       />
-      <div className="xl-px-8 relative mx-4 overflow-hidden py-10 font-roboto font-normal md:px-6 lg:px-6">
+      <Toast setShow={setShowToast} show={showToast} toastData={toastData} />
+      <div className="xl-px-8 relative mx-4 overflow-hidden pt-10 pb-4 font-roboto font-normal md:px-6 lg:px-6">
         <div className="flex flex-col space-y-4 divide-y divide-slate-900">
           <div className="m-0 mx-auto max-w-prose text-lg text-slate-800">
             <EditorContent editorState={editorState} />
           </div>
           {/* Comment Section */}
-          <div className="mx-auto flex w-full max-w-none flex-col md:w-full lg:w-1/2">
+          <div className="mx-auto w-full max-w-none flex-1 flex-col md:w-full lg:w-1/2">
+            {/* First row in column - Comments title and order by menu */}
             <div className="my-2 grid grid-cols-2">
               <p className="justify-self-start">Comments</p>
               <p className="mr-2 justify-self-end">Order by</p>
             </div>
-            <div className="my-2 grid max-h-screen grid-cols-1 divide-y divide-stone-300 overflow-y-scroll md:max-h-[30rem] lg:max-h-[40rem]">
-              {comments.map((comment, index) => {
-                return (
-                  <BlogComment
-                    content={comment.content}
-                    user={props.userProps.userData}
-                    upVotes={comment.upVotes}
-                    downVotes={comment.downVotes}
-                    key={index}
-                  />
-                );
-              })}
+            {/* Second row in col - Actual scrollable comments section */}
+            {props.comments?.length > 0 && (
+              <div className="my-2 grid max-h-screen grid-cols-1 divide-y divide-stone-300 overflow-y-scroll md:max-h-[30rem] lg:max-h-[40rem]">
+                {props.comments.map((comment, index) => {
+                  return <BlogCommentItem comment={comment} user={props.userProps.userData} key={index} />;
+                })}
+              </div>
+            )}
+
+            {(!props.comments || props.comments?.length === 0) && (
+              <p className="mt-6 text-slate-500">No comments have been added yet</p>
+            )}
+            {/* Third row in col - text to add comment or comment editor section */}
+            <div className="my-6 flex flex-row md:my-2">
+              {!props.userProps.user ? (
+                <p className="mt-2">
+                  <span className="cursor-pointer pr-2 text-blue-500 hover:text-blue-400">
+                    <Link href={'/login'}>Log in</Link>
+                  </span>
+                  to post comments
+                </p>
+              ) : (
+                !showCommentEditor && (
+                  <p
+                    className="mt-2 cursor-pointer text-blue-500 hover:text-blue-400"
+                    onClick={() => setShowCommentEditor(true)}
+                  >
+                    {usersComment?.length === 0 ? 'Add Comment' : 'Edit Comment'}
+                  </p>
+                )
+              )}
+              {showCommentEditor && (
+                <AddCommentSection
+                  user={props.userProps.userData}
+                  usersComment={usersComment.length === 0 ? null : usersComment[0]}
+                  setShowCommentEditor={setShowCommentEditor}
+                  toast={{setShowToast, setToastData}}
+                  blogId={props.path}
+                />
+              )}
             </div>
-            <p className="my-2 cursor-pointer text-blue-500 hover:text-blue-400">Add Comment</p>
           </div>
         </div>
       </div>
     </main>
   );
 }
+
+interface AddCommentSectionProps {
+  setShowCommentEditor: Dispatch<SetStateAction<boolean>>;
+  user: UserData;
+  toast: {
+    setShowToast: Dispatch<SetStateAction<boolean>>;
+    setToastData: Dispatch<SetStateAction<ToastData>>;
+  };
+  blogId: string;
+  usersComment: BlogComment;
+}
+
+const AddCommentSection = (props: AddCommentSectionProps) => {
+  const [editorState, setEditorState] = useState(() => {
+    if (props.usersComment) {
+      const contentState = convertFromRaw(JSON.parse(props.usersComment.body));
+
+      return EditorState.createWithContent(contentState);
+    } else {
+      return EditorState.createEmpty();
+    }
+  });
+
+  async function SubmitComment() {
+    const defaultAvatar = `https://firebasestorage.googleapis.com/v0/b/rafael-zasas.appspot.com/o/default-avatar.jpg?alt=media&token=da5befb3-193c-4a14-a17b-f036828dbf5b`;
+    const wordCount = editorState.getCurrentContent().getPlainText('\u0001').trim().split(/\s+/).length;
+
+    if (wordCount >= 10) {
+      const comment: BlogComment = {
+        author: {
+          email: props.user.email,
+          permissions: {admin: props.user.permissions.admin, level: props.user.permissions.level ?? 0},
+          uid: props.user.uid,
+          username: props.user.username,
+          profilePhoto: props.user.profilePhoto || props.user.photoURL || defaultAvatar,
+        },
+        id: props.user.uid,
+        body: JSON.stringify(convertToRaw(editorState.getCurrentContent())),
+        createdAt: serverTimestamp(),
+        updatedAt: serverTimestamp(),
+        upVotes: [],
+        downVotes: [],
+      };
+
+      try {
+        await addBlogComment(props.blogId, comment);
+        props.setShowCommentEditor(false);
+        props.toast.setToastData({
+          heading: `Thanks ${props.user.username?.split(' ')[0] ?? props.user.email.split('@')[0]}`,
+          body: 'Your thoughts are greatly apprecated',
+          type: 'success',
+        });
+
+        props.toast.setShowToast(true);
+        setTimeout(() => {
+          props.toast.setShowToast(false);
+        }, 3000);
+
+        setEditorState(EditorState.push(editorState, ContentState.createFromText(''), 'remove-range'));
+      } catch (error) {
+        console.log(error);
+        props.toast.setToastData({
+          heading: `Error`,
+          body: error,
+          type: 'error',
+        });
+
+        props.toast.setShowToast(true);
+        setTimeout(() => {
+          props.toast.setShowToast(false);
+        }, 3000);
+      }
+    } else {
+      props.toast.setToastData({
+        heading: 'Comment is too short',
+        body: 'Thoughtful comments should be 10 words at least',
+        type: 'error',
+      });
+
+      props.toast.setShowToast(true);
+      setTimeout(() => {
+        props.toast.setShowToast(false);
+      }, 3000);
+    }
+  }
+
+  return (
+    <div className="mt-4 h-full flex-1 flex-col">
+      <div className="z-0 flex h-3/4 w-full flex-1 flex-row">
+        <TextEditor editorState={editorState} setEditorState={setEditorState} placeholder={'Enter comment...'} />
+      </div>
+      <div className="gird-cols-2 mt-4 grid gap-2 md:grid-cols-2">
+        <div className="mx-0 justify-self-stretch md:justify-self-start">
+          <Button
+            text={props.usersComment ? 'Edit' : 'Post'}
+            type="button"
+            function={SubmitComment}
+            rightIcon={faFeather}
+          />
+        </div>
+        <p
+          className="cursor-pointer text-blue-500 hover:text-blue-400 md:justify-self-end"
+          onClick={() => props.setShowCommentEditor(false)}
+        >
+          Cancel
+        </p>
+      </div>
+    </div>
+  );
+};
