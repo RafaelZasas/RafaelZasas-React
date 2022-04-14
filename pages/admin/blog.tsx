5d7dc0abf602271fb83d5821e45cc9e5b72627ef
Rafael <zasas.rafi@gmail.com>
2022-04-13 22:01:02 -0700

    ✨ (Admin Blog Page): Add image uploader for blog posts

--- pages/admin/blog.tsx
+++ pages/admin/blog.tsx
@@ -1,5 +1,5 @@
 import {ContentState, convertToRaw, EditorState} from 'draft-js';
-import React, {FormEvent, useContext, useState} from 'react';
+import React, {Dispatch, FormEvent, SetStateAction, useContext, useState} from 'react';
 import Spinner1 from '../../components/loadingSpinners/Spinner1';
 import TextEditor from '../../components/textEditor/TextEditor';
 import {ToastContext, UserContext} from '../../lib/context';
@@ -11,21 +11,25 @@ import {GetTags, PostBlog} from '../../lib/FirestoreOperations';
 import {BlogPost, BlogTag} from '../../lib/types';
 import {serverTimestamp} from 'firebase/firestore';
 import Metatags from '../../components/Metatags';
+import CustomImage from '../../components/Image';
+import FileInputButton from '../../components/FileInputButton';
+import {uploadImage} from '../../lib/CloudStorageOperations';
 
 function TitleInput() {
   return (
     <div className="">
-      <label htmlFor="title" className="block text-sm font-medium text-gray-700">
+      <label htmlFor="title" className="block text-sm font-medium text-gray-700 dark:text-slate-200">
         Title
       </label>
       <div className="mt-1">
         <input
           type="title"
           name="title"
+          maxLength={60}
           autoFocus
           id="title"
-          className="block h-9 w-full rounded-md border-2 border-gray-300 bg-gray-100/30 p-2 shadow-sm
-           backdrop-blur-xl backdrop-filter hover:border-blue-400/70 focus:border-blue-500 sm:text-sm"
+          className="block h-9 w-full rounded-md border-2 border-gray-300 bg-gray-100/30 p-2 shadow-sm backdrop-blur-xl backdrop-filter hover:border-blue-400/70
+           focus:border-blue-500 dark:border-slate-500 dark:bg-black/30 dark:text-white sm:text-sm"
           placeholder="My fancy title"
         />
       </div>
@@ -36,19 +40,75 @@ function TitleInput() {
 function SummaryInput() {
   return (
     <div>
-      <label htmlFor="summary" className="block text-sm font-medium text-gray-700">
+      <label htmlFor="summary" className="block text-sm font-medium text-gray-700 dark:text-slate-200">
         Summary
       </label>
       <div className="mt-1">
         <textarea
           rows={4}
           name="summary"
+          maxLength={160}
           id="summary"
           className="block w-full rounded-md border-2 border-gray-300 bg-gray-100/30 p-2 shadow-sm
-          backdrop-blur-xl backdrop-filter hover:border-blue-400/70 focus:border-blue-500 sm:text-sm"
+          backdrop-blur-xl backdrop-filter hover:border-blue-400/70 focus:border-blue-500 dark:border-slate-500 dark:bg-black/30 dark:text-white sm:text-sm"
           defaultValue={''}
         />
       </div>
+    </div>
+  );
+}
+
+interface ImageInputProps {
+  postImage: string;
+  setPostImage: Dispatch<SetStateAction<string>>;
+  postImageName: string;
+  setPostImageName: Dispatch<SetStateAction<string>>;
+}
+
+function ImageInput(props: ImageInputProps) {
+  const addImage = (e) => {
+    let files = e.target.files;
+
+    let fileName = files[0].name;
+
+    props.setPostImageName(fileName);
+    let fileReader = new FileReader();
+    fileReader.addEventListener('load', () => {
+      uploadImage(`card-tiles/${fileName}`, fileReader.result as string).then((imageUrl) => {
+        console.log(imageUrl);
+        props.setPostImage(imageUrl);
+      });
+    });
+    fileReader.readAsDataURL(files[0]);
+  };
+
+  return (
+    <div className="flex flex-col space-y-5">
+      <label className="block text-sm font-medium text-gray-700 dark:text-slate-200">Image</label>
+
+      {props.postImage && (
+        <span className="inline-block h-fit w-fit overflow-hidden bg-gray-100 dark:bg-gray-800">
+          <CustomImage
+            src={props.postImage}
+            alt={props.postImageName}
+            width={'100%'}
+            height={'100%'}
+            className="h-48 w-full object-cover"
+            layout={'intrinsic'}
+            objectFit={'contain'}
+          />
+        </span>
+      )}
+      <label className="my-2" htmlFor="imageIput">
+        <FileInputButton
+          type="button"
+          text={props.postImage ? 'Edit' : 'Add'}
+          buttonStyle="info"
+          id={'imageInput'}
+          name={'imgageInput'}
+          handleOnChange={addImage}
+        />
+      </label>
     </div>
   );
 }
@@ -60,6 +120,8 @@ export default function BlogPage({}) {
   const [isDraft, setIsDraft] = useState(true);
   const tags = GetTags();
   const {setShowToast, setToastData} = useContext(ToastContext);
+  const [postImage, setPostImage] = useState<string | null>(null);
+  const [postImageName, setPostImageName] = useState<string>('postImage');
 
   function AddTag(tag: BlogTag) {
     if (selectedTags.filter((obj) => obj.id === tag.id).length === 0) {
@@ -96,6 +158,10 @@ export default function BlogPage({}) {
       downVotes: [],
     };
 
+    if (!!postImage) {
+      postData['displayImage'] = postImage;
+    }
+
     try {
       PostBlog(postData);
 
@@ -111,6 +177,8 @@ export default function BlogPage({}) {
         e.target.reset();
         // reset the editor
         setEditorState(EditorState.push(editorState, ContentState.createFromText(''), 'remove-range'));
+        setSelectedTags([]);
+        setPostImage('');
       }, 3000);
     } catch (error) {
       setToastData({
@@ -130,7 +198,7 @@ export default function BlogPage({}) {
     return !userData?.permissions?.admin ? (
       <DefaultErrorPage statusCode={404} />
     ) : (
-      <main>
+      <main className="dark:bg-gray-800 dark:text-slate-200">
         <Metatags
           title="Admin Blog Management"
           description="Shhh. This page is a secret"
@@ -144,6 +212,12 @@ export default function BlogPage({}) {
           <div className="z-10 my-2 w-full flex-1 flex-row space-y-2 md:w-1/2">
             <TitleInput />
             <SummaryInput />
+            <ImageInput
+              postImage={postImage}
+              setPostImage={setPostImage}
+              postImageName={postImageName}
+              setPostImageName={setPostImageName}
+            />
             <div className="flex flex-col">
               <ComboBox items={tags} label={'Tags'} function={AddTag} />
               <div className="my-3 flex flex-wrap space-x-2 md:flex-row">
