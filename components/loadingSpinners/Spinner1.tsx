edf6950f3cf68312fd8fca1454522aebab3f7d82
Rafael <zasas.rafi@gmail.com>
2022-04-18 21:04:51 -0700

    💄 (Loading Spinner): Change to white on dark mode

--- components/loadingSpinners/Spinner1.tsx
+++ components/loadingSpinners/Spinner1.tsx
@@ -2,13 +2,9 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSpinner} from '@fortawesome/free-solid-svg-icons';
 
 export default function Spinner1() {
-    return (
-        <div className={'my-3 flex flex-col items-center'}>
-            <FontAwesomeIcon icon={faSpinner}
-                             pulse
-                             fixedWidth
-                             size='4x'/>
-        </div>
-    );
+  return (
+    <div className={'my-3 flex flex-col items-center dark:text-slate-200'}>
+      <FontAwesomeIcon icon={faSpinner} pulse fixedWidth size="4x" />
+    </div>
+  );
 }
-
